03233f1d5cdbe756525f021f11095d7b6f68d3de
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-28 21:21:13 -0500

    added core components and updating App.js with routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import NewProject from "./components/NewProject";
+import Footer from "./components/Footer";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/" element={<NewProject />} />
+          </Routes>
+          <Footer />
+    </Router>
   );
 }
 
